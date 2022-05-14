27e1114b69d81f8995d4e87e69d5dc37f5d9d79f
pimper <pimper906@gmail.com>
2022-05-14 14:12:51 +0900

    unity game
    학원에서 배운것들

---
+++ RollADice/Assets/Scripts/Tile/TileInfo_Inverse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+public class TileInfo_Inverse : TileInfo
+{
+    public override void TileEvent()
+    {
+        Debug.Log($"index of this tile : {index} , player will move inversed direction once ");
+        DicePlayManager.instance.direction = -1;
+    }
+}
